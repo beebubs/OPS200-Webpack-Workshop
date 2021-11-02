e35b3f6afa9a5e3fc7e4081c958f37726a075a35
Alex Martinez <alexkmartinez77@gmail.com>
2021-11-02 14:55:38 -0700

    Changes index.js as instructed and adds several babel devDependencies needed to make it work.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import $ from 'jquery';
+// var $ = require('jquery');
 
-const rootElement = document.getElementById('root');
-const sampleComponent = <div> React and webpack are working! </div>;
-
-ReactDOM.render(sampleComponent, rootElement);
+$('div').text('Webpack is working!');
